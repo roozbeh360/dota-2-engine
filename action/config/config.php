c4ebd74f9a9a2bf9bc936bd16e08736da15b5676
roozbeh baabakaan <baabakaan@remake.ir>
2013-01-20 20:45:30 +0330

    api key changed

--- action/config/config.php
+++ action/config/config.php
@@ -8,7 +8,7 @@ class config{
 	static public  $api_match_history = "GetMatchHistory";
 	static public  $api_match_details = "GetMatchDetails";
 	static public  $api_version = array('V001','V002');
-	static public  $api_key = "B6F432DA5034276589FA896956AD8234";
+	static public  $api_key = "B6F432D___your key____956AD8234";
 }
    
 
