47fe4ac21d45689335a8936db8cabfcac2a894ad
roozbeh baabakaan <baabakaan@remake.ir>
2013-01-30 16:07:30 +0330

    esc curl added

--- core/lib/curl.library.php
+++ core/lib/curl.library.php
@@ -12,23 +12,33 @@ class curLoad{
 	private $curl_errno , $curl_error ;
 	public $raw_response ;
 	
-	function __construct($query) {
+	function __construct($query,$fast=true) {
 
-		$ch = curl_init(); 
-	    curl_setopt( $ch, CURLOPT_URL, $query ); 
-	    @curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true ); 
-	    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true ); 
-	    curl_setopt( $ch, CURLOPT_AUTOREFERER, true ); 
-	    curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false ); 
-	    curl_setopt( $ch, CURLOPT_MAXREDIRS, 5 ); 
-	    curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 10 ); 
-	    $raw_response = curl_exec( $ch ); 
-	    $http_response_header = curl_getinfo( $ch ); 
-	    $curl_errno = curl_errno($ch); 
-	    $curl_error = curl_error($ch); 
-	    curl_close($ch); 
-	     
-		$this->raw_response = $raw_response ;
+		if($fast)
+		{
+			
+			$ch = curl_init(); 
+		    curl_setopt( $ch, CURLOPT_URL, $query ); 
+		    @curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true ); 
+		    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true ); 
+		    curl_setopt( $ch, CURLOPT_AUTOREFERER, true ); 
+		    curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false ); 
+		    curl_setopt( $ch, CURLOPT_MAXREDIRS, 5 ); 
+		    curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 10 ); 
+		    $raw_response = curl_exec( $ch ); 
+		    $http_response_header = curl_getinfo( $ch ); 
+		    $this->curl_errno = curl_errno($ch); 
+		    $this->curl_error = curl_error($ch); 
+		    curl_close($ch);
+			$this->raw_response = $raw_response ;
+		}
+		else
+		{
+			$raw_response = file_get_contents($query);
+			$this->raw_response = $raw_response ;
+		} 
+
+		
 	}
 	
 	public function getResponse()
@@ -55,9 +65,4 @@ class curLoad{
 		
 	}
 	
-}  
- 
-   
-     
-    
-    
+}
